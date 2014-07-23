1bd4a9e64fe9492c0ca177a59e58d304c24219aa
rotoshine <rotoshine@gmail.com>
2014-07-23 21:06:53 +0900

    동기화...

--- server/routes/repository.js
+++ server/routes/repository.js
@@ -3,7 +3,7 @@ var repository = require('../controllers/repository');
 module.exports = function(app) {
     app.get('/api/repository', repository.repository);
     app.get('/api/repository/github', repository.getGIthubRepository);
-    app.post('/api/repository/sync', repository.syncAll);
+    app.post('/api/repository/sync', repository.syncFromGithub);
 
     // app.post('/repository/sync/:repositoryName', repository.syncOne);
 };
