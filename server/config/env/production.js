df81d927c140b90f5c9ded065d4b85ca4160edca
rotoshine <rotoshine@gmail.com>
2014-07-23 20:47:58 +0900

    운영설정 추가

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -5,29 +5,31 @@ module.exports = {
     app: {
         name: 'flyService'
     },
-    facebook: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
-    },
-    twitter: {
-        clientID: 'CONSUMER_KEY',
-        clientSecret: 'CONSUMER_SECRET',
-        callbackURL: 'http://localhost:3000/auth/twitter/callback'
-    },
     github: {
         clientID: 'd64bfb89cc502f5461ca',
         clientSecret: '6ad15b8c0e4b01543bf317197767b855d6548097',
         callbackURL: 'http://join.flyjenkins.org/auth/github/callback'
     },
-    google: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/google/callback'
-    },
-    linkedin: {
-        clientID: 'API_KEY',
-        clientSecret: 'SECRET_KEY',
-        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    flyJenkinsCoreAPI: {
+        serviceGroupList: {
+            url: 'http://core.flyjenkins.org/jenkins/plugin/flyJenkins/serviceGroupList',
+            method: 'GET',
+            params: {
+                page: 1,
+                limit: 5
+            }
+        },
+        createServiceGroup: {
+            url: 'http://core.flyJenkins.org/jenkins/plugin/flyJenkins/saveServiceGroup',
+            method: 'POST'
+        },
+        serviceList: {
+            url: 'http://core.flyjenkins.org/jenkins/plugin/flyJenkins/serviceList',
+            method: 'GET',
+            params: {
+                page: 1,
+                limit: 5
+            }
+        }
     }
 };
